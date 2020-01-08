5ded7ae83c651d62abcde8557ea1ae55d921d911
Garald-48rus <56934346+Garald-48rus@users.noreply.github.com>
2020-01-08 19:05:03 +0300

    Create Value.h

---
+++ Value.h
@@ -0,0 +1,18 @@
+#ifndef VALUE_H
+#define VALUE_H
+
+//класс флага работы курсора
+class Value
+{
+public:
+    Value();
+     void setHoper(int);
+
+    bool getHoper();
+
+private:
+
+    bool mHoper;
+};
+
+#endif // VALUE_H
